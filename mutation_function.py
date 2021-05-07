aa2c88f9fb8f1600dccba0dfa1aeab8bc44e2af5
pangwoon <nkw096@naver.com>
2021-05-07 14:21:00 +0900

    feat: mutation

--- mutation_function.py
+++ mutation_function.py
@@ -13,11 +13,7 @@ def inversion(offspring):
     end = [] if ind2==len(offspring)-1 else offspring[ind2+1:]
     return offspring[:ind1] + list(reversed(offspring[ind1:ind2+1])) + end
 
-
-
 def swap(offspring):
     ind1, ind2 = np.random.choice([i for i in range(len(offspring))], 2, replace=False)
     offspring[ind1], offspring[ind2] = offspring[ind2], offspring[ind1]
     return offspring
-
-print(inversion([0,1,2,3,4,5,6,7,8,9]))
