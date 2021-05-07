9e8ee95ccee156c73757f38250c2fa3eb2fd3339
Mountain96 <jtlsan@naver.com>
2021-05-07 17:20:56 +0900

    Update Crossovers

--- Crossovers.py
+++ Crossovers.py
@@ -26,8 +26,8 @@ class Crossover:
 
     @staticmethod
     def pmx(parent1, parent2):
-        point1 = int(len(parent1) / 3)
-        point2 = point1 * 2
+        point1 = random.randint(int(len(parent1)/10), len(parent1) - 2)
+        point2 = random.randint(point1 + 1, int(len(parent2)*9/10))
         np1 = np.array(parent1)
         np2 = np.array(parent2)
         
